e72aaa44b73fbcfb1b35a5c692f6dc7219f381f8
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-10-17 21:14:13 +0530

    P 01 - Section 04 (Building APIs) - Updating an Object

--- 001-Part-One/next-app/app/api/users/[id]/route.tsx
+++ 001-Part-One/next-app/app/api/users/[id]/route.tsx
@@ -12,3 +12,17 @@ export async function GET(request: NextRequest, { params }: Props) {
   }
   return NextResponse.json({ id: id, name: "bob" });
 }
+
+export async function PUT(request: NextRequest, { params }: Props) {
+  const body = await request.json();
+  if (!body.name) {
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  }
+
+  const { id } = await params;
+  if (id > 10) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+
+  return NextResponse.json({ id: id, name: body.name });
+}
