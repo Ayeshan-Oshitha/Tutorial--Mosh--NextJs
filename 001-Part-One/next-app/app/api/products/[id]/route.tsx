55d340dcd1c34b6b00ef486182d802722ff4d3fa
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-10-23 20:32:21 +0530

    P 01 - Section 04 (Building APIs) - Building Products API

---
+++ 001-Part-One/next-app/app/api/products/[id]/route.tsx
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
+
+interface Props {
+  params: { id: number };
+}
+
+export async function GET(request: NextRequest, { params }: Props) {
+  const { id } = await params;
+  if (id > 10) {
+    return NextResponse.json({ error: "Product not found" }, { status: 404 });
+  }
+  return NextResponse.json({ id: id, name: "Rice", price: 25.0 });
+}
+
+export async function PUT(request: NextRequest, { params }: Props) {
+  const body = await request.json();
+  const validation = schema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.issues, { status: 400 });
+  }
+
+  const { id } = await params;
+  if (id > 10) {
+    return NextResponse.json({ error: "Product not found" }, { status: 404 });
+  }
+
+  return NextResponse.json({ id: id, name: body.name, price: body.price });
+}
+
+export async function DELETE(request: NextRequest, { params }: Props) {
+  const { id } = await params;
+  if (id > 10) {
+    return NextResponse.json({ error: "Product not found" }, { status: 404 });
+  }
+  return NextResponse.json(
+    { message: `Product with id ${id} deleted successfully` },
+    { status: 200 }
+  );
+}
---
+++ 001-Part-One/next-app/app/api/products/route.tsx
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
+
+export function GET(request: NextRequest) {
+  return NextResponse.json([
+    { id: 1, name: "Milk", price: 10.0 },
+    { id: 2, name: "Bread", price: 20.0 },
+    { id: 3, name: "Sugar", price: 30.0 },
+  ]);
+}
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = schema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.issues, { status: 400 });
+  }
+  return NextResponse.json(
+    { id: 1, name: body.name, price: body.price },
+    { status: 201 }
+  );
+}
