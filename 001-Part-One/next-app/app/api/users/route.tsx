4bf49c77e147c8b4c7f3f399992169c3364ab497
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-10-23 23:39:32 +0530

    P 01 - Section 05 (Database Integration with Prisma) - Creating Data

--- 001-Part-One/next-app/app/api/users/route.tsx
+++ 001-Part-One/next-app/app/api/users/route.tsx
@@ -14,5 +14,24 @@ export async function POST(request: NextRequest) {
   if (!validation.success) {
     return NextResponse.json(validation.error.issues, { status: 400 });
   }
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+
+  const exisitingUser = await prisma.user.findUnique({
+    where: { email: body.email },
+  });
+
+  if (exisitingUser) {
+    return NextResponse.json(
+      { error: "User with this email already exists" },
+      { status: 409 }
+    );
+  }
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(newUser, { status: 201 });
 }
