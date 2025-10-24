d5c81247fea2a96b895cd00f9ea539d876910c8b
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-10-24 11:04:17 +0530

    P 01 - Section 06 (Uploading Files) - Showing Uploaded Images

--- 001-Part-One/next-app/app/upload/page.tsx
+++ 001-Part-One/next-app/app/upload/page.tsx
@@ -1,12 +1,37 @@
 "use client";
 
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState<string | null>(null);
+
   return (
     <>
-      <CldUploadWidget uploadPreset="next_app_rgt">
+      {publicId && (
+        <CldImage
+          width="270"
+          height="180"
+          crop="fill"
+          src={publicId}
+          alt="Uploaded Image"
+        />
+      )}
+      <CldUploadWidget
+        uploadPreset="next_app_rgt"
+        onSuccess={(results) => {
+          console.log("Upload successful! Here is the image info: ", results);
+          if (results.event !== "success") {
+            return;
+          }
+          const info = results.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
         {({ open }) => {
           return (
             <button className="btn btn-primary" onClick={() => open()}>
