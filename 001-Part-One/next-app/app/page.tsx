bf0d155bfb16410176386eb108f86e92660783ca
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-10-25 19:51:18 +0530

    P 01 - Section 07 (Authentication with Next Auth) - Accessing Session on the Server

--- 001-Part-One/next-app/app/page.tsx
+++ 001-Part-One/next-app/app/page.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main className="flex flex-col gap-y-3">
-      <h1>Hello World</h1>
+      <h1>Hello {session && <span>{session.user?.name}</span>}</h1>
       <Link href="/users">Go to Users Page</Link>
       <ProductCard />
     </main>
