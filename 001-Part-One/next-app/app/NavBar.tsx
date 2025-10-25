51af00a72a8211829d3671bc538e4c6efe5af9ee
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-10-25 19:17:10 +0530

    P 01 - Section 07 (Authentication with Next Auth) - Accessing Sessions on the Client

--- 001-Part-One/next-app/app/NavBar.tsx
+++ 001-Part-One/next-app/app/NavBar.tsx
@@ -1,13 +1,21 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   return (
     <div className="flex bg-slate-200 p-5 mb-4 space-x-5">
       <Link href="/">Next.js</Link>
       <Link href="/users">Users</Link>
       <Link href="/admin">Admin</Link>
-      <Link href="/api/auth/signin">Login</Link>
+      {status === "loading" && <div>Loading...</div>}
+      {status === "authenticated" && <div>{session.user?.name}</div>}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin">Login</Link>
+      )}
     </div>
   );
 };
