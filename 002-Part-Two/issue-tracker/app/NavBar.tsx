79a5945fa76ca200b16f9def3c68e1c99167b4ed
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-11-26 14:28:37 +0530

    P 02 - Section 06 (Authentication) - Adding the Login and Logout Links

--- 002-Part-Two/issue-tracker/app/NavBar.tsx
+++ 002-Part-Two/issue-tracker/app/NavBar.tsx
@@ -4,9 +4,12 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { FaBug } from "react-icons/fa";
 import classNames from "classnames";
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
 
 const NavBar = () => {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
 
   const links = [
     {
@@ -27,19 +30,28 @@ const NavBar = () => {
         </Link>
         <ul className="flex space-x-6">
           {links.map((link) => (
-            <Link
-              key={link.href}
-              href={link.href}
-              className={classNames({
-                "text-zinc-900": link.href === currentPath,
-                "text-zinc-500": link.href !== currentPath,
-                "hover:text-zinc-800 transition-colors": true,
-              })}
-            >
-              {link.label}
-            </Link>
+            <li key={link.href}>
+              <Link
+                href={link.href}
+                className={classNames({
+                  "text-zinc-900": link.href === currentPath,
+                  "text-zinc-500": link.href !== currentPath,
+                  "hover:text-zinc-800 transition-colors": true,
+                })}
+              >
+                {link.label}
+              </Link>
+            </li>
           ))}
         </ul>
+        <Box>
+          {status == "authenticated" && (
+            <Link href="/api/auth/signout">Log Out</Link>
+          )}
+          {status == "unauthenticated" && (
+            <Link href="/api/auth/signin">Login</Link>
+          )}{" "}
+        </Box>
       </nav>
     </>
   );
--- 002-Part-Two/issue-tracker/app/layout.tsx
+++ 002-Part-Two/issue-tracker/app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
 import { Container, Theme } from "@radix-ui/themes";
+import AuthProvider from "./auth/Provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,12 +26,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme accentColor="violet" appearance="light">
-          <NavBar />
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-        </Theme>
+        <AuthProvider>
+          <Theme accentColor="violet" appearance="light">
+            <NavBar />
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
