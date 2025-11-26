f7dab317ac7dbb8c7c4144c181572e397c9044f9
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-11-26 18:21:10 +0530

    P 02 - Section 07 (Assigning Issues to User) - Assigning an Issue to a User

--- 002-Part-Two/issue-tracker/app/issues/[id]/page.tsx
+++ 002-Part-Two/issue-tracker/app/issues/[id]/page.tsx
@@ -45,7 +45,7 @@ const DetailPage = async ({ params }: Props) => {
         {session && (
           <Box>
             <Flex direction="column" gap="3">
-              <AssigneeSelect />
+              <AssigneeSelect issue={issue} />
               <EditIssueButton issueId={issue.id} />
               <DeleteIssueButton issueId={issue.id} />
             </Flex>
